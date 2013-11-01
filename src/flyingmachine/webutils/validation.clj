d9b91ac7410842288feb37c4d0e4a76046c957e9
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-10-31 20:02:34 -0400

    more cleanup

--- src/flyingmachine/webutils/validation.clj
+++ src/flyingmachine/webutils/validation.clj
@@ -1,9 +1,9 @@
 (ns ^{:doc "validation: combination of field name and validation checks
 
-validation check group: first member is an error message, the rest
-of the members are validation checks. If any validation check
-returns false then the error message is added to a list of error
-messages for the given field.
+validation check group: a seq of alternating error messages and
+validation functions. If a validation function returns false then the
+error message is added to a list of error messages for the given
+field.
 
 validation check: a function to apply to the value corresponding to
 the field name specified in the validation"}
@@ -16,28 +16,27 @@ the field name specified in the validation"}
    validation-check-groups is a seq of alternating messages and
    validation checks"
   [value validation-check-groups]
-  (for [[error-message validation] (partition 2 validation-check-groups)
-        :when (not (validation value))]
+  (for [[error-message validation-check] (partition 2 validation-check-groups)
+        :when (not (validation-check value))]
     error-message))
 
 (defn validate
   "returns a map with a vec of errors for each key"
   [to-validate validations]
   (loop [errors {}
-         v (seq validations)]
-    (if-let [validation (first v)]
+         remaining-validations (seq validations)]
+    (if-let [validation (first remaining-validations)]
       (let [[fieldname validation-check-groups] validation
             value (get to-validate fieldname)
             error-messages (error-messages-for value validation-check-groups)]
         (if (empty? error-messages)
-          (recur errors (rest v))
-          (recur (assoc errors fieldname error-messages) (rest v))))
+          (recur errors (rest remaining-validations))
+          (recur (assoc errors fieldname error-messages) (rest remaining-validations))))
       errors)))
 
 (defmacro if-valid
+  "Handle validation more concisely"
   [to-validate validations errors-name & then-else]
-  `(let [to-validate# ~to-validate
-         validations# ~validations
-         ~errors-name (validate to-validate# validations#)]
+  `(let [~errors-name (validate ~to-validate ~validations)]
      (if (empty? ~errors-name)
        ~@then-else)))
